de7998d07ca3b958fe8e2a86c9e2eecaefe5a3df
Lily <Lily@163.com>
2022-12-06 14:30:50 +0800

    划分目录结构

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  transpileDependencies: true
+  // transpileDependencies: true
+  configureWebpack: {
+    resolve: {
+      // extensions:[], 在里面可以配置，这样有些路径的文件后缀名可以不写
+      // 起别名
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views'
+
+      }
+    }
+  }
 }
